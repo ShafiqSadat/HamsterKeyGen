896c9ba8a8cbeb595831f8aa7892c96259322c83
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-08-21 17:42:42 +0430

    Added Polysphere game!

--- main.py
+++ main.py
@@ -58,6 +58,13 @@ games = {
         'promoId': '61308365-9d16-4040-8bb0-2f4a4c69074c',
         'timing': 20000 / 1000,
         'attempts': 20,
+    },
+      7: {
+        'name': 'Polysphere',
+        'appToken': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        'promoId': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        'timing': 20000 / 1000,
+        'attempts': 20,
     }
 }
 
