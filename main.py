2150fbe945ae171f49c18ec3548acf1cbafeaccb
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-08-28 13:12:22 +0430

    Added support for Cafe Dash game!

--- main.py
+++ main.py
@@ -32,51 +32,51 @@ games = {
         'attempts': 20,
     },
     3: {
-        'name': 'My Clone Army',
-        'appToken': '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        'promoId': 'fe693b26-b342-4159-8808-15e3ff7f8767',
-        'timing': 180000 / 1000,
-        'attempts': 30,
-    },
-    4: {
         'name': 'Train Miner',
         'appToken': '82647f43-3f87-402d-88dd-09a90025313f',
         'promoId': 'c4480ac7-e178-4973-8061-9ed5b2e17954',
         'timing': 20000 / 1000,
         'attempts': 15,
     },
-    5: {
+    4: {
         'name': 'Merge Away',
         'appToken': '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
         'promoId': 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
         'timing': 20000 / 1000,
         'attempts': 25,
     },
-    6: {
+    5: {
         'name': 'Twerk Race 3D',
         'appToken': '61308365-9d16-4040-8bb0-2f4a4c69074c',
         'promoId': '61308365-9d16-4040-8bb0-2f4a4c69074c',
         'timing': 20000 / 1000,
         'attempts': 20,
     },
-      7: {
+      6: {
         'name': 'Polysphere',
         'appToken': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         'promoId': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         'timing': 20000 / 1000,
         'attempts': 20,
     },
-        8: {
+        7: {
         'name': 'Mow and Trim',
         'appToken': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
         'promoId': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
         'timing': 20000 / 1000,
         'attempts': 20,
     },
-        9: {
+      8: {
         'name': 'Mud Racing',
         'appToken': '8814a785-97fb-4177-9193-ca4180ff9da8',
         'promoId': '8814a785-97fb-4177-9193-ca4180ff9da8',
+        'timing': 20000 / 1000,
+        'attempts': 20,
+    },
+        9: {
+        'name': 'Cafe Dash',
+        'appToken': 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        'promoId': 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
         'timing': 20000 / 1000,
         'attempts': 20,
     }
