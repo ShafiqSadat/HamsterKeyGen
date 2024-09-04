52d54cee1469edfa484169eafe869186514253cd
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2024-09-04 12:31:37 +0430

    Added support for Fluff Crusade and Tile Trio!

--- main.py
+++ main.py
@@ -60,9 +60,9 @@ games = {
         'attempts': 20,
     },
     7: {
-        'name': 'Cafe Dash',
-        'appToken': 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-        'promoId': 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        'name': 'Tile Trio',
+        'appToken': 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+        'promoId': 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
         'timing': 20000 / 1000,
         'attempts': 20,
     },
@@ -74,10 +74,10 @@ games = {
         'attempts': 20,
     },
     9: {
-        'name': 'Gangs Wars',
-        'appToken': 'b6de60a0-e030-48bb-a551-548372493523',
-        'promoId': 'c7821fa7-6632-482c-9635-2bd5798585f9',
-        'timing': 40000 / 1000,
+        'name': 'Fluff Crusade',
+        'appToken': '112887b0-a8af-4eb2-ac63-d82df78283d9',
+        'promoId': '112887b0-a8af-4eb2-ac63-d82df78283d9',
+        'timing': 30000 / 1000,
         'attempts': 20,
     },
 }
